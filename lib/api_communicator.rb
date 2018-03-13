15762f948ffe1e9beb1542ad906e2b64a03c78ef
azeelleeza <leezaluncheon@gmail.com>
2018-03-13 15:47:25 -0400

    refactored

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,32 +2,27 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+def get_characters
+  people_api = 'http://www.swapi.co/api/people/'
+  JSON.parse(RestClient.get(people_api))["results"]
+end
+
 def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  if get_characters.none?{|hashobj|hashobj["name"]==character}
+     puts "no such character"
+     exit
+   else
+    film_array=get_characters.find{|hashobj|hashobj["name"]==character}["films"]
+    film_array.map{ |film_api| JSON.parse(RestClient.get(film_api)) }
+   end
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  puts films_hash.map{|film| film["title"]}.join(", ")
 end
 
 def show_character_movies(character)
+  get_characters
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
