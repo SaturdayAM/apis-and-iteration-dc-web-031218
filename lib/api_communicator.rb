72085d8859952dc7548fe31f70e7889a3cecf529
azeelleeza <leezaluncheon@gmail.com>
2018-03-13 21:25:27 -0400

    adjusted for paging api (han solo)

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,27 +2,36 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_characters
+
+def next_page(count)
   people_api = 'http://www.swapi.co/api/people/'
-  JSON.parse(RestClient.get(people_api))["results"]
+  current_page = JSON.parse(RestClient.get("#{people_api}?page=#{count}"))
+  if current_page["next"]!= nil
+    current_page["results"]
+  else
+    puts "No such character."
+    do_it
+  end
 end
 
 def get_character_movies_from_api(character)
-  if get_characters.none?{|hashobj|hashobj["name"]==character}
-     puts "no such character"
-     exit
-   else
-    film_array=get_characters.find{|hashobj|hashobj["name"]==character}["films"]
-    film_array.map{ |film_api| JSON.parse(RestClient.get(film_api)) }
-   end
+  found = nil
+  count = 1
+
+  until found
+    found = next_page(count).find{|hashobj|hashobj["name"]==character}
+    count +=1
+  end
+
+  found["films"].map{ |film_api| JSON.parse(RestClient.get(film_api))} if found !=nil
 end
 
+
 def parse_character_movies(films_hash)
-  puts films_hash.map{|film| film["title"]}.join(", ")
+  puts "\n#{films_hash.map{|film| film["title"]}.join("\n").upcase}\n"
 end
 
 def show_character_movies(character)
-  get_characters
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,9 +1,10 @@
 def welcome
-puts "welcome"
+puts "Welcome to..."
+puts  "    8888888888  888    88888\n   88     88   88 88   88  88\n    8888  88  88   88  88888\n       88 88 888888888 88   88\n88888888  88 88     88 88    888888\n\n 88  88  88   888    88888    888888\n88  88  88  88 88   88  88  88\n88 8888 88 88   88  88888    8888\n 888  888 888888888 88   88     88\n  88  88  88     88 88    8888888"
 end
 
 def get_character_from_user
-  puts "please enter a character"
-gets.chomp
+  puts "\nPlease enter a character (or 'q' to quit):"
+  gets.chomp
 
 end
